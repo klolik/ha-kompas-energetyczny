f329d2b8c482dbbd99abc73a804f515d7ee00faa
klolik <klolik79@gmail.com>
2025-03-11 21:31:16 +0100

    Config flow could not be loaded: {"message":"Invalid handler specified"}

--- custom_components/kompas_energetyczny/config_flow.py
+++ custom_components/kompas_energetyczny/config_flow.py
@@ -2,7 +2,7 @@ import logging
 from homeassistant import config_entries
 from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowResultType
-import homeassistant.util.voluptuous_serialize as vol
+import voluptuous as vol
 from .const import DOMAIN, DEFAULT_URL
 
 _LOGGER = logging.getLogger(__name__)
@@ -19,6 +19,7 @@ class KompasEnergetycznyConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 step_id="user",
                 data_schema=vol.Schema({vol.Optional("url", default=DEFAULT_URL): str}),
             )
+
         await self.async_set_unique_id(DOMAIN)
         self._abort_if_unique_id_configured()
         return self.async_create_entry(title="Kompas Energetyczny", data={"url": user_input["url"]})
