9d370deaf97edf49d201191859b64cd0b7bd9c91
klolik <klolik79@gmail.com>
2025-03-11 21:16:14 +0100

    downgrade to v0.0.1

--- custom_components/kompas_energetyczny/config_flow.py
+++ custom_components/kompas_energetyczny/config_flow.py
@@ -11,6 +11,9 @@ class KompasEnergetycznyConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        if self._async_current_entries():
+            return self.async_abort(reason="single_instance_allowed")
+
         if user_input is None:
             return self.async_show_form(
                 step_id="user",
