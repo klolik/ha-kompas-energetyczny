d162eb1bd32275f98da843b9443ebc26a7c80cc4
klolik <klolik79@gmail.com>
2025-10-23 11:29:46 +0200

    Make export share negative for consistency

--- custom_components/kompas_energetyczny/sensor.py
+++ custom_components/kompas_energetyczny/sensor.py
@@ -136,7 +136,7 @@ class KompasEnergetycznyPowerImportShareSensor(KompasEnergetycznyBaseSensor):
         if zapotrzebowanie > generacja: # import as a % of demand
             return 100 * (zapotrzebowanie - generacja) / zapotrzebowanie
         if zapotrzebowanie < generacja: # export as a % of supply
-            return 100 * (generacja - zapotrzebowanie) / generacja
+            return 100 * (zapotrzebowanie - generacja) / generacja # negative
         return None # Should never happen
 
     @property
