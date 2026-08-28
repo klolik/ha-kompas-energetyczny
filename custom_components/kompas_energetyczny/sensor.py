5866f9dbb037e03a2878fee9dd306ada7f683eba
klolik <klolik79@gmail.com>
2026-08-28 15:41:54 +0000

    Change power suggested display precision to 0 to match API

--- custom_components/kompas_energetyczny/sensor.py
+++ custom_components/kompas_energetyczny/sensor.py
@@ -62,6 +62,7 @@ class KompasEnergetycznyBaseSensor(SensorEntity):
         self._attr_unique_id = f"{self.api_data.coordinator.config_entry.entry_id}_{sid}"
         self._attr_state_class = SensorStateClass.MEASUREMENT
         self._attr_device_info = self.api_data.device
+        self._attr_suggested_display_precision = 0
 
     async def async_added_to_hass(self) -> None:
         self.async_on_remove(
