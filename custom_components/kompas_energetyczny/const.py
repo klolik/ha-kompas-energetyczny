70650bd094b8e01684426417315ae0c2f3a48ed7
klolik <klolik79@gmail.com>
2026-08-30 11:08:42 +0000

    Update flows URL to vanity domain

--- custom_components/kompas_energetyczny/const.py
+++ custom_components/kompas_energetyczny/const.py
@@ -8,7 +8,7 @@ HOME_URL = "https://www.energetycznykompas.pl/"
 # Value rounding precision, namely percentage
 PRECISION = 2
 
-API_URL_PRZESYLY = "https://kompasen-dcgbapbjg3fkb5gp.a01.azurefd.net/datafile/przesyly.json"
+API_URL_PRZESYLY = "https://files.energetycznykompas.pl/datafile/przesyly.json"
 API_URL_RAPORTY_V2 = 'https://api.raporty.pse.pl/api/pdgsz?$filter=dtime%20gt%20%27{}%27%20and%20is_active%20eq%20true'
 
 STATUS_MAP = {
