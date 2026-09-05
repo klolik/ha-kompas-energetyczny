1f16d153600883f8aa6016a775e0d6d4bb749c77
klolik <klolik79@gmail.com>
2026-09-05 10:36:43 +0000

    Change required_restriction to required_reduction

--- custom_components/kompas_energetyczny/const.py
+++ custom_components/kompas_energetyczny/const.py
@@ -12,10 +12,10 @@ API_URL_PRZESYLY = "https://files.energetycznykompas.pl/datafile/przesyly.json"
 API_URL_RAPORTY_V2 = 'https://api.raporty.pse.pl/api/pdgsz?$filter=dtime%20gt%20%27{}%27%20and%20is_active%20eq%20true'
 
 STATUS_MAP = {
-    0: "recommended_use",      # Zalecane uzytkowanie
-    1: "normal_use",           # Normalne uzytkowanie
-    2: "recommended_saving",   # Zalecane oszczedzanie
-    3: "required_restriction", # Wymagane ograniczenie
+    0: "recommended_use",    # Zalecane uzytkowanie
+    1: "normal_use",         # Normalne uzytkowanie
+    2: "recommended_saving", # Zalecane oszczedzanie
+    3: "required_reduction", # Wymagane ograniczenie
 }
 
 STATUS_MAP_SHORT = {
